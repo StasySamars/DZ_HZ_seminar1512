2688622625aa0cbdfe7eb581944c722e87c7bcc6
StasySamars <119619342+StasySamars@users.noreply.github.com>
2022-12-15 23:03:03 +0300

    задача 1 готова

---
+++ work1/Program.cs
@@ -0,0 +1,16 @@
+﻿// 1 вариант записи
+
+//Console.Clear();
+
+//Console.Write("Введите число: ");
+//int a = int.Parse(Console.ReadLine());
+//Console.WriteLine("Квадрат " + Convert.ToString(a) + "=" + (a * a));
+
+// int number = Convert.ToInt32(Console.ReadLine()); можно использовать вместо int.Parse
+
+// второй вариант записи
+Console.Clear();
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine($"Квадрат {a} = {a * a}");
---
+++ zadacha1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее
+
+Console.Clear();
+Console.Write("Привет. Как тебя зовут? ");
+string username = (Console.ReadLine());
+
+Console.WriteLine(username + ", введи первое любое число:");
+int Num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(username + ", введи второе любое число:");
+int Num2 = Convert.ToInt32(Console.ReadLine());
+
+if (Num1 < Num2)
+{
+    Console.WriteLine($"Максимальное значение = {Num2} , Минимальное значение = {Num1} ");
+
+}
+else
+{
+    Console.WriteLine($"Минимальное значение = {Num1} , Максимальное значение = {Num2} ");
+
+}
+
+Console.WriteLine(username + ", Вы восхитительны!)");
