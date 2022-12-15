949a6b19a4995c10e6bfee841ed0b4b6c0761faf
StasySamars <119619342+StasySamars@users.noreply.github.com>
2022-12-16 00:37:13 +0300

    3 задача решена

---
+++ zadacha3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка).
+Console.Clear();
+Console.Write("Рад Вас видеть! Как я могу к Вам обращаться? ");
+string username = (Console.ReadLine());
+
+Console.WriteLine(username + ", введите любое число и мы узнаем, четное оно или нет: ");
+int Num1 = Convert.ToInt32(Console.ReadLine());
+
+if (Num1%2 == 0)
+Console.WriteLine(username + ", введенное Вами число, является четным! ");
+
+else
+Console.WriteLine(username + ", введенное Вами число, является НЕчетным!");
+
+Console.WriteLine("Если хотите попробовать еще раз, просто введите команду dotnet run");
